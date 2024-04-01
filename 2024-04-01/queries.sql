09ea9f279a1a8472a421f55d2a4af914badb3da5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-01 12:06:48 -0500

    Scripted auto-commit on change (2024-04-01 12:06:48) by gitwatch.sh

--- 2024-04-01/queries.sql
+++ 2024-04-01/queries.sql
@@ -1,5 +1,4 @@
-SELECT A.iid, I.name, MAX(A.servings)
-  FROM assignments A, ingredients I
-  WHERE A.iid=I.iid
- GROUP BY A.iid, I.name
-HAVING COUNT(*) >= 2;
+  SELECT I.iid, I.name, COUNT(*) AS count
+    FROM ingredients I, assignments A
+    WHERE I.iid=A.iid AND I.cal >= 100
+   GROUP BY I.iid, I.name;
