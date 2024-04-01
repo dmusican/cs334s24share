72e287f15c4c40a1b86b41523c835f3490064119
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-01 14:45:35 -0500

    Scripted auto-commit on change (2024-04-01 14:45:35) by gitwatch.sh

--- 2024-04-01/queries.sql
+++ 2024-04-01/queries.sql
@@ -1,10 +1,5 @@
-select R.name
-   from recipes R, assignments A
-
-          where R.rid
-
-          = A.rid
-
-        AND A.iid = 3
-
-        ;
+   SELECT R.name
+     FROM recipes R,
+          assignments A
+    WHERE R.rid = A.rid
+      AND A.iid = 3;
