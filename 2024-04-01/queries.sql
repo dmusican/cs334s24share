a5e319356e362972c118c034e99d58e0a6914100
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-01 11:48:56 -0500

    Scripted auto-commit on change (2024-04-01 11:48:56) by gitwatch.sh

--- 2024-04-01/queries.sql
+++ 2024-04-01/queries.sql
@@ -3,8 +3,8 @@
      FROM ingredients I,
           assignments A,
           recipes R
-    WHERE I.iid = A.iid,
-          R.rid = A.rid,
+    WHERE I.iid = A.iid AND
+          R.rid = A.rid AND
           (
           R.name = 'Spiced Pumpkin Soup'
        OR R.name = 'Sugar Cookie Icing'
