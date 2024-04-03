bdd61656696537797d08ea470e0b235b19b56d14
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-03 14:56:16 -0500

    Scripted auto-commit on change (2024-04-03 14:56:16) by gitwatch.sh

--- 2024-04-03/queries.sql
+++ 2024-04-03/queries.sql
@@ -2,7 +2,7 @@
    SELECT max(R.year) as maxyear
      FROM recipes R;
 
-select I.iid, I.name, max(A.servings)
+select I.iid, I.name, max(A.servings) as maxservings
 from   assignments A, ingredients I
 where  A.iid=I.iid
 group by I.iid, I.name;
