f82d0bb77a1fd6719723a4700783602ba3604c59
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-03 14:58:59 -0500

    Scripted auto-commit on change (2024-04-03 14:58:59) by gitwatch.sh

--- 2024-04-03/queries.sql
+++ 2024-04-03/queries.sql
@@ -6,4 +6,4 @@ select I.iid, I.name, max(A.servings) as maxservings
 from   assignments A, ingredients I
 where  A.iid=I.iid
 group by I.iid, I.name
-having max(A.servings) > 2;
+where max(A.servings) > 2;
