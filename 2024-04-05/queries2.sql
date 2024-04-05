0e0ce3518d3049b041ad7a2c3fdbc15ce7f54591
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-05 14:40:10 -0500

    Scripted auto-commit on change (2024-04-05 14:40:10) by gitwatch.sh

--- 2024-04-05/queries2.sql
+++ 2024-04-05/queries2.sql
@@ -3,6 +3,6 @@
  --INSERT INTO recipes values (6, 'Cake', 7.9, 2000);
  --INSERT INTO recipes values (7, 'Donut', 7.2, 2005);
  
-SELECT R.rid, R.name
+SELECT R.rid, R.name, A.aid
   FROM recipes R
   LEFT OUTER JOIN assignments A USING (rid);
