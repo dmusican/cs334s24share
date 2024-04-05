770421fdf92e0f4ec9a934b3bee4f53c13b6228e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-05 14:38:05 -0500

    Scripted auto-commit on change (2024-04-05 14:38:05) by gitwatch.sh

--- 2024-04-05/queries.sql
+++ 2024-04-05/queries.sql
@@ -15,14 +15,5 @@ SELECT R.name
   JOIN assignments A USING (rid)
  WHERE A.rid=3;
 
- -- Outer join: keep rows form one relation that are not 
- -- in the other
- --INSERT INTO recipes values (6, 'Cake', 7.9, 2000);
- --INSERT INTO recipes values (7, 'Donut', 7.2, 2005);
  
-SELECT R.name
-  FROM recipes R
-  JOIN assignments A USING (rid)
- WHERE A.rid=3;
 
- 
