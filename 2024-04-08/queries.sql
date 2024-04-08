ab2340a73602368b5d14f2e1e8f02f30622eafac
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-08 14:05:30 -0500

    Scripted auto-commit on change (2024-04-08 14:05:30) by gitwatch.sh

--- 2024-04-08/queries.sql
+++ 2024-04-08/queries.sql
@@ -9,3 +9,9 @@ INSERT INTO immediate_prereqs VALUES ('CS201', 'CS257');
 INSERT INTO immediate_prereqs VALUES ('CS257', 'CS347');
 INSERT INTO immediate_prereqs VALUES ('CS201', 'CS251');
 INSERT INTO immediate_prereqs VALUES ('CS201', 'CS344');
+
+-- Just to get second level of prereqs
+SELECT 
+FROM immediate_prereqs IP1,
+     immediate_prereqs IP2
+WHERE IP1.course_id = IP2.prereq_id;
