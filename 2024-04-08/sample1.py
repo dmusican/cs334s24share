54a783b3eeedc882638ea3e992f37cdbb55acfe8
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-04-08 14:39:26 -0500

    Scripted auto-commit on change (2024-04-08 14:39:26) by gitwatch.sh

--- 2024-04-08/sample1.py
+++ 2024-04-08/sample1.py
@@ -4,13 +4,12 @@ conn = psycopg2.connect("user=csuser dbname=csuser")
 
 cur = conn.cursor()
 
-query = "SELECT * FROM RECIPES";
-
 name = input('Which recipe? ')
 
-# query = """SELECT *
-#              FROM recipes
-#             WHERE name='"""  + name + "'"
+# The bad way
+query = """SELECT *
+             FROM recipes
+            WHERE name='"""  + name + "'"
 
 #print(query)-
 cur.execute(query)
